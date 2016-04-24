ddc0e292ac1e37e2882bd5686b4fabc2ac9acb02
austin <austin@austins-MacBook.local>
2016-04-24 09:11:45 -0700

    Adding reference points to the xml for each of the textViews and creating an array to store them in

--- TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -5,14 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Creating a reference for my buttons
+    TextView button1, button2, button3, button4, button5, button6, button7, button8, button9;
+
+    //Creating a button/TextView array to store the buttons
+    TextView[] buttonArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Linking the buttons/textViews to the xml
+
+        button1 = (TextView) findViewById(R.id.textView);
+        button2 = (TextView) findViewById(R.id.textView2);
+        button3 = (TextView) findViewById(R.id.textView3);
+        button4 = (TextView) findViewById(R.id.textView4);
+        button5 = (TextView) findViewById(R.id.textView5);
+        button6 = (TextView) findViewById(R.id.textView6);
+        button7 = (TextView) findViewById(R.id.textView7);
+        button8 = (TextView) findViewById(R.id.textView8);
+        button9 = (TextView) findViewById(R.id.textView9);
+
+        //Instantiating the textView array
+        buttonArray = new TextView[]{button1, button2, button3, button4,  button5, button6, button7,  button8, button9};
+
+
 
     }
 }
