653af0246cb480d82cc79ad05fe947853311325e
austin <austin@austins-MacBook.local>
2016-04-24 15:13:48 -0700

    Adding XML and other changes

--- TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -1,6 +1,7 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.view.View.OnClickListener; //Imports the ability to implement OnC
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.sql.SQLOutput;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     //Creating a reference for my buttons
@@ -24,8 +27,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     //Creates an initial counter for the turns and sets it to 0
     int turn_count = 0;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +35,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         gameMessage = (TextView) findViewById(R.id.game_message_text);
         //gameMessage.setText(p);
         //Setting the textview to the initial first players name!
-        gameMessage.setText(getIntent().getExtras().getString("player1Name")+ " " + "goes first!");
-
+        gameMessage.setText(getIntent().getExtras().getString("player1Name") + " " + "goes first!");
 
         //Linking the buttons/textViews to the xml
         button1 = (TextView) findViewById(R.id.textView);
@@ -47,15 +47,14 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         button7 = (TextView) findViewById(R.id.textView7);
         button8 = (TextView) findViewById(R.id.textView8);
         button9 = (TextView) findViewById(R.id.textView9);
-        newGame = (Button)findViewById(R.id.newGame);
+        newGame = (Button) findViewById(R.id.newGame);
         //Instantiating the textView array
-        buttonArray = new TextView[]{button1, button2, button3, button4,  button5, button6, button7,  button8, button9};
+        buttonArray = new TextView[]{button1, button2, button3, button4, button5, button6, button7, button8, button9};
         //Creates a loop for every textView to take an action when clicked
-        for(TextView looppingTextViews : buttonArray){
+        for (TextView looppingTextViews : buttonArray) {
             //Sets the onClick and uses the class as the handler for the event
             looppingTextViews.setOnClickListener(this);
         }
-
         //Creates onClick listener for the newGame button
         newGame.setOnClickListener(new OnClickListener() {
             @Override
@@ -64,10 +63,10 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 turn = true;
                 turn_count = 0;
                 resetGame(true);
-
             }
         });
     }
+
     @Override
     public void onClick(View v) {
         //method called when person clicks
@@ -77,21 +76,21 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         buttonClicked(newButton);
     }
 
-    public void buttonClicked(TextView newButton){
+    public void buttonClicked(TextView newButton) {
 
         //A boolean that changes the text on the textview to either x or o depending on whose turn it is
-        if(turn){
-            gameMessage.setText(getIntent().getExtras().getString("player2Name") + "'s" + " "+ "turn!");
+        if (turn) {
+            gameMessage.setText(getIntent().getExtras().getString("player2Name") + "'s" + " " + "turn!");
             //X's turn
             newButton.setText("X");
             //changes the color of the textView to blue when player x chooses it
             newButton.setBackgroundColor(Color.BLUE);
-        }else {
-            gameMessage.setText(getIntent().getExtras().getString("player1Name") + "'s" + " "+ "turn!");
+        } else {
+            gameMessage.setText(getIntent().getExtras().getString("player1Name") + "'s" + " " + "turn!");
             //O's turn
             newButton.setText("O");
             //changes the color of the textView to red when player o chooses it
-            newButton.setBackgroundColor(Color.RED);
+            newButton.setBackgroundColor(Color.MAGENTA);
         }
         //just the increment counter
         turn_count++;
@@ -104,13 +103,13 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         checkForWinner();
     }
 
-    private void checkForWinner(){
+    public void checkForWinner() {
 
         boolean there_is_a_winner = false;
 
         //checks horozontal numbers
         //by adding that the first button is not clickable it got it to work instead of always saying one is winning when clicked
-        if(button1.getText() == button2.getText() && button2.getText() == button3.getText() && !button1.isClickable())
+        if (button1.getText() == button2.getText() && button2.getText() == button3.getText() && !button1.isClickable())
             there_is_a_winner = true;
         else if (button4.getText() == button5.getText() && button5.getText() == button6.getText() && !button4.isClickable())
             there_is_a_winner = true;
@@ -118,7 +117,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             there_is_a_winner = true;
 
         //vertical
-        if(button1.getText() == button4.getText() && button4.getText() == button7.getText() && !button1.isClickable())
+        if (button1.getText() == button4.getText() && button4.getText() == button7.getText() && !button1.isClickable())
             there_is_a_winner = true;
         else if (button2.getText() == button5.getText() && button5.getText() == button8.getText() && !button2.isClickable())
             there_is_a_winner = true;
@@ -126,49 +125,83 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             there_is_a_winner = true;
 
         //Digonal
-        if(button1.getText() == button5.getText() && button5.getText() == button9.getText() && !button1.isClickable())
+        if (button1.getText() == button5.getText() && button5.getText() == button9.getText() && !button1.isClickable())
             there_is_a_winner = true;
         else if (button3.getText() == button5.getText() && button5.getText() == button7.getText() && !button3.isClickable())
             there_is_a_winner = true;
 
-        if(there_is_a_winner)
-        {
+
+        if (there_is_a_winner) {
             //because turn is reversed up at the top, you have to reverse the boolean, hence the !
-            if(!turn){
+            if (!turn) {
                 toast("Player 1 wins!");
-            }else{
+                String player1won = new String("Player 1 won previous game");
+
+            } else {
                 toast("Player 2 wins!");
+                String player2won = new String("Player 2 won previous game");
+
+
             }
             //generic method call
             resetGame(false);
 
-        }else if(turn_count == 9){
+        } else if (turn_count == 9) {
             //when all buttons are used and no longer useable
             toast("No one wins sadly");
+            String draw = new String("It was a draw");
+
+
         }
+
     }
+
     //Creates method to reset the game if a player wins
-    private void resetGame(boolean enable){
-       //false to disable
-        for (TextView reset: buttonArray){
+    private void resetGame(boolean enable) {
+        //false to disable
+        for (TextView reset : buttonArray) {
             reset.setClickable(enable);
 
             //for the color
             if (enable) {
                 reset.setBackgroundColor(Color.GRAY);
                 //Resetting default name to the original first player
-                gameMessage.setText(getIntent().getExtras().getString("player1Name")+ " " + "goes first!");
+                gameMessage.setText(getIntent().getExtras().getString("player1Name") + " " + "goes first!");
                 //reset the text to be an empty string
                 reset.setText(" ");
 
 
-            }else
+            } else
                 reset.setBackgroundColor(Color.GREEN);
         }
     }
-    private String toast(String message){
+
+    private String toast(String message) {
         //Making sure the buttons are clickable by creating a toast message
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
         return message;
     }
+
+
+//    private String winningPlayer() {
+//        if (checkForWinner() == "Player 1 won previous game"){
+//            return player1won;
+//
+//        }else if (checkForWinner() == "Player 2 won previous game"){
+//            String player2won = new String ("Player 2 won previous game");
+//            return player2won;
+//
+//        }
+//
+//        String draw = new String ("It was a draw!");
+//        return draw;
+//
+//    }
+
+    private String sendingOverNameFromSP() {
+        SharedPreferences prefs = getSharedPreferences("WinningPlayer", MODE_PRIVATE);
+        String winningPlayer = prefs.getString("winning player", "Player 1 won previous match");
+        return winningPlayer;
+    }
+
 }
--- TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -13,19 +13,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     //Creating new instances of EditText and String to references below
     EditText player1, player2;
     String player1GetText, player2GetText;
     TextView player1tv, player2tv;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     //Creates and intent to sending the user from this activity onto the next.
     //OnClick created in the XML for ease ;)
 
@@ -35,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         player1 = (EditText) findViewById(R.id.player_one_name);
         player2 = (EditText) findViewById(R.id.player_two_name);
         //Getting the text from the editText field.
-        player1GetText= new String(player1.getText().toString());
+        player1GetText = new String(player1.getText().toString());
         player2GetText = new String(player2.getText().toString());
 
         Intent intent = new Intent(MainActivity.this, GameActivity.class);
@@ -43,27 +40,23 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("player2Name", player2.getText().toString());
         startActivity(intent);
 
-
-
-
         //Creating a reference for the textViews which show the players name
-        player1tv =  (TextView) findViewById(R.id.player1tv);
-        player2tv =  (TextView) findViewById(R.id.player2tv);
+        player1tv = (TextView) findViewById(R.id.player1tv);
+        player2tv = (TextView) findViewById(R.id.player2tv);
         //Setting the text of the EditText fields to reflect the two players input
         player1tv.setText(player1GetText);
         player2tv.setText(player2GetText);
         //Calling both function to send over using SP
 //        sendingOverNameFromSP();
 //        storeNamesToSP();
-
     }
 
-//    private String sendingOverNameFromSP() {
-//        SharedPreferences prefs = getSharedPreferences("PlayersNames", MODE_PRIVATE);
-//        String player1Coming = prefs.getString(player1GetText, "No name sent");
-//       // String player2Coming = prefs.getString(player2GetText, null);
-//        return player1Coming;
-//    }
+    private String sendingOverNameFromSP() {
+        SharedPreferences prefs = getSharedPreferences("PlayersNames", MODE_PRIVATE);
+        String player1Coming = prefs.getString(player1GetText, "No name sent");
+        // String player2Coming = prefs.getString(player2GetText, null);
+        return player1Coming;
+    }
 //
 //    private void storeNamesToSP(){
 //        SharedPreferences prefs = getSharedPreferences("PlayersNames", MODE_PRIVATE);
