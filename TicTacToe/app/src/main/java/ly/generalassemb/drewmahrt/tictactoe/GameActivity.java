fcb8d603d5e783d5b5b1fbe339f4ec31e5fbe147
austin <austin@austins-MacBook.local>
2016-04-24 23:41:25 -0700

    Adding the toast messages to be a longer length

--- TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ TicTacToe/app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -142,12 +142,12 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         if (there_is_a_winner) {
             //because turn is reversed up at the top, you have to reverse the boolean, hence the !
             if (!turn) {
-                toast("Play Again?");
+                toast("Play Again? ..Click New Game!");
                 gameMessage.setText("Player 1 wins!");
                 String player1won = new String("Player 1 won previous game");
 
             } else {
-                toast("Play Again?");
+                toast("Play Again? ..Click New Game!");
                 gameMessage.setText("Player 2 wins!");
                 String player2won = new String("Player 2 won previous game");
 
@@ -159,8 +159,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         } else if (turn_count == 9) {
             //when all buttons are used and no longer useable
-            toast("Play Again?");
-            gameMessage.setText("No one wins sadly");
+            toast("Play Again? ..Click New Game!");
+            gameMessage.setText("It's a draw! New Game?");
             String draw = new String("It was a draw");
 
 
